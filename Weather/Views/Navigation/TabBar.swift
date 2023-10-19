588cd8d292a044f52b99f3be4e29ab2044092419
Aasif <imaasif28@gmail.com>
2023-10-19 19:43:58 +0530

    Tab Bar minor changes

--- Weather/Views/Navigation/TabBar.swift
+++ Weather/Views/Navigation/TabBar.swift
@@ -10,44 +10,49 @@ import SwiftUI
 struct TabBar: View {
     var action: ()-> Void
     var body: some View {
-        ZStack{
-            //MARK: Arc Shape
-            
+        ZStack {
+            // MARK: Arc Shape
             Arc()
                 .fill(Color.tabBarBackground)
                 .frame(height: 88)
-                .overlay{
-                Arc()
-                        .stroke(Color.tabBarBorder,lineWidth: 0.5)
+                .overlay {
+                    // MARK: Arc Border
+                    Arc()
+                        .stroke(Color.tabBarBorder, lineWidth: 0.5)
                 }
-            HStack{
+            
+            // MARK: Tab Items
+            HStack {
+                // MARK: Expand Button
                 Button {
                     action()
                 } label: {
                     Image(systemName: "mappin.and.ellipse")
-                        .frame(width: 44,height: 44)
+                        .frame(width: 44, height: 44)
                 }
-
+                
                 Spacer()
-                NavigationLink{
+                
+                // MARK: Navigation Button
+                NavigationLink {
                     WeatherView()
-                    
                 } label: {
                     Image(systemName: "list.star")
-                        .frame(width: 44,height: 44)
+                        .frame(width: 44, height: 44)
                 }
-            }.font(.title2)
-                .foregroundColor(.white)
-                .padding(EdgeInsets(top: 20, leading: 32, bottom: 24, trailing: 32))
+            }
+            .font(.title2)
+            .foregroundColor(.white)
+            .padding(EdgeInsets(top: 20, leading: 32, bottom: 24, trailing: 32))
         }
-        .frame(maxHeight: .infinity ,alignment: .bottom)
+        .frame(maxHeight: .infinity, alignment: .bottom)
         .ignoresSafeArea()
-    
     }
 }
 
-struct Navigation_Previews: PreviewProvider {
+struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
-        TabBar(action: {}).preferredColorScheme(.dark)
+        TabBar(action: {})
+.preferredColorScheme(.dark)
     }
 }
